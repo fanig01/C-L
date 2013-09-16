df12c14959cec6a663550f4e8192c3e8cda87498
Geison de Souza Oliveira <geisonszo@gmail.com>
2013-09-15 21:51:19 -0300

    comentrios e indentacao

--- cel/aplicacao/seguranca.php
+++ cel/aplicacao/seguranca.php
@@ -1,6 +1,8 @@
 <?php
 
-// Escapa os metacaracteres do PHP
+/*
+ *  Escapa os metacaracteres do PHP
+ */
 
 function escapa_metacaracteres($string) {
     $string = ereg_replace("[][{}()*+?.\\^$|]", "\\\\0", $string);
@@ -8,17 +10,22 @@ function escapa_metacaracteres($string) {
 }
 
 function prepara_dado($string) {
-    //Remove espa�os em branco do inicio e do fim da string
-    //$string = trim( $string );
-    // Substitui o & por amp; (para que n�o de problemas ao gerar o XML)
-
+    /*
+     * Remove espaços em branco do inicio e do fim da string
+     * $string = trim( $string );
+     * Substitui o & por amp; (para que não de problemas ao gerar o XML)
+     */
     $string = ereg_replace("&", "&amp;", $string);
 
-    // Retira tags html e php da string
+    /*
+     *  Retira tags html e php da string
+     */
 
     $string = strip_tags($string);
 
-    // Verifica se a diretiva get_magic_quotes_gpc() esta ativada, se estiver a fun��o stripslashes � utilizada na string
+    /*
+     *  Verifica se a diretiva get_magic_quotes_gpc() esta ativada, se estiver a função stripslashes é utilizada na string
+     */
 
     $string = get_magic_quotes_gpc() ? stripslashes($string) : $string;
     $string = function_exists("mysql_real_escape_string") ? mysql_real_escape_string($string) : mysql_escape_string($string);
