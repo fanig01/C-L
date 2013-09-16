4c38b69368f651bbc0f7daf6cb88c5993c137541
Geison de Souza Oliveira <geisonszo@gmail.com>
2013-09-15 21:18:07 -0300

    Comentario e indentacao do arquivo

--- cel/aplicacao/rmv_conceito.php
+++ cel/aplicacao/rmv_conceito.php
@@ -1,12 +1,26 @@
 <?php
-// rmv_conceito.php: Este script faz um pedido de remover um conceito do projeto.
-// Arquivo chamador: main.php
-
+/*
+ * rmv_conceito.php: Este script faz um pedido de remover um conceito do projeto.
+ * Arquivo chamador: main.php
+ * 
+ * Cenário  -    Excluir Conceito 
+ * Objetivo:	 Permitir ao Usuário Excluir um conceito que esteja ativo
+ * Contexto:	 Usuário deseja excluir um conceito
+ * Pró-Condição: Login, cenário cadastrado no sistema
+ * Atores:	 Usuário, Sistema
+ * Recursos:	 Dados informados
+ * Epis�dios:	 O sistema fornecer uma tela para o usuário justificar a necessidade daquela
+ *               exclusão para que o administrador possa ler e aprovar ou não a mesma.
+ *               Esta tela tamb�m conter um botão para a confirmaçao da exclusão.
+ *               Restrição: Depois de clicar no botão, o sistema verifica se todos os campos foram preenchidos 
+ * Exceção:	 Se todos os campos não foram preenchidos, retorna para o usuário uma mensagem
+ *               avisando que todos os campos devem ser preenchidos e um botão de voltar para a pagina anterior.
+ */
 session_start();
 
 include("funcoes_genericas.php");
 include("httprequest.inc");
-checkUserAuthentication("index.php");        // Checa se o usuario foi autenticado
+checkUserAuthentication("index.php");
 
 inserirPedidoRemoverConceito($_SESSION['id_projeto_corrente'], $id_conceito, $_SESSION['id_usuario_corrente']);
 ?>  
@@ -16,24 +30,9 @@ inserirPedidoRemoverConceito($_SESSION['id_projeto_corrente'], $id_conceito, $_S
     opener.parent.frames['code'].location.reload();
     opener.parent.frames['text'].location.replace('main.php?id_projeto=<?= $_SESSION['id_projeto_corrente'] ?>');
 
-<?php
-// Cen�rio -  Excluir Conceito 
-//Objetivo:	Permitir ao Usu�rio Excluir um conceito que esteja ativo
-//Contexto:	Usu�rio deseja excluir um conceito
-//              Pr�-Condi��o: Login, cen�rio cadastrado no sistema
-//Atores:	Usu�rio, Sistema
-//Recursos:	Dados informados
-//Epis�dios:	O sistema fornecer� uma tela para o usu�rio justificar a necessidade daquela
-//              exclus�o para que o administrador possa ler e aprovar ou n�o a mesma.
-//              Esta tela tamb�m conter� um bot�o para a confirma��o da exclus�o.
-//              Restri��o: Depois de clicar no bot�o, o sistema verifica se todos os campos foram preenchidos 
-//Exce��o:	Se todos os campos n�o foram preenchidos, retorna para o usu�rio uma mensagem
-//              avisando que todos os campos devem ser preenchidos e um bot�o de voltar para a pagina anterior.
-?>
-
 </script>
 
-<h4>Operação efetuada com sucesso!</h4>
+<h4>Opera&ccedil;&atilde;o efetuada com sucesso!</h4>
 
 <script language="javascript1.3">
 
