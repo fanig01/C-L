9b80fcb064a9a8bfb21fb55d931ebdfb350577fe
Tomaz Martins <tomaz.unb@outlook.com>
2013-09-15 22:31:02 -0300

    Alterações: comentários e "parágrafos" de código

--- cel/aplicacao/coloca_tags_xml.php
+++ cel/aplicacao/coloca_tags_xml.php
@@ -1,9 +1,9 @@
 <?php
 
 /* * ******************************************************
- * Modulo criado em 05/07/07
- * Grupo PES_07_1_1
- * Autores:
+ * Module created in 05/07/07
+ * Group PES_07_1_1
+ * Authors:
  *   BVF
  *   DFS
  *   TVD
@@ -11,7 +11,7 @@
  * ****************************************************** */
 
 /* * ******************************************************* 
-  /* Modulo que poe as tags dos links no arquivo XML
+  /* Module that put the tags of links into the archive XML
   /******************************************************** */
 
 
@@ -27,8 +27,10 @@ function pega_id_xml($str) {
 
     $j = 0;
     $i = 0;
+    
     while ($str[$i] != '*') {
         $buffer[$j] = $str[$i];
+        
         $i++;
         $j++;
     }
@@ -41,61 +43,85 @@ function troca_chaves_xml($str) {
     $conta_fehados = 0;
     $comeco;
     $fim;
-    $x = 0;
+    
+    $realLinks = 0;
     $y = 0;
+    
     $vet_id;
     $link_original;
     $link_novo;
+    
     $buffer3 = '';
     $buffer = 0;
+    
     $i = 0;
+    
     $tam_str = strlen($str);
 
     while ($i <= $tam_str) {
         if ($str[$i] == '}') {
             $conta_abertos = $conta_abertos + 1;
         }
+       
         $i++;
-    } // FIM WHILE 1
+    }
+    
     $i = 0;
+    
     while ($i <= $tam_str) {
         if ($str[$i] == '}') {
             $conta_fechados = $conta_fechados + 1;
         }
+        
         $i++;
-    } // FIM WHILE 2
+        
+    }
+    
     $i = 0;
+    
     if ($conta_abertos == 0) {
         return $str;
     }
+    
     $i = 0;
+    
     while ($i <= $tam_str) {
+        
         if ($str[$i] == '{') {
             $buffer = $buffer + 1;
+            
             if ($buffer == 1) {
-                $comeco[$x] = $i;
-                $x++;
+                $comeco[$realLinks] = $i;
+                
+                $realLinks++;
             }
         }
-        if ($str[$i] == '}') {
+        
+        if ($str[$i] == '}') {    
             $buffer = $buffer - 1;
+           
             if ($buffer == 0) {
                 $fim[$y] = $i + 1;
+                
                 $y++;
             }
         }
+        
         $i++;
     };
+    
     $i = 0;
 
-    while ($i < $x) { //x = numero de links reais - 1    
+    while ($i < $realLinks) { 
         $link = substr($str, $comeco[$i], $fim[$i] - $comeco[$i]);
         $link_original[$i] = $link;
+        
         $link = str_replace('{', '', $link);
         $link = str_replace('}', '', $link);
         $buffer2 = 0;
         $conta = 0;
         $n = 0;
+       
         //echo('aki - >'."$link".'<br>');
         $vet_id[$i] = pega_id_xml($link);
         $link = '**' . $link;
@@ -121,17 +147,22 @@ function troca_chaves_xml($str) {
             if ($marcador == 1) {
                 $link[$n] = '{';
             }
+            
             $n++;
         }
+        
         $link = str_replace('{', '', $link);
         $link = poe_tag_xml($link, $vet_id[$i]);
         $link_novo[$i] = $link;
         $i++;
     }
+    
     $i = 0;
+    
     //echo("STRING INICAL -> $str<br/>");
-    while ($i < $x) {
+    while ($i < $realLinks) {
         $str = str_replace($link_original[$i], $link_novo[$i], $str);
+       
         $i++;
     }
     //echo("STRING FINAL -> $str<br/>");
