0362ab30751fcb83b88133952adfe2f1e7d746cc
Thaiane Braga <thaiane.unb@hotmail.com>
2013-09-25 11:39:26 -0300

    Desfazendo as alterações no arquivo heading.php

--- cel/aplicacao/heading.php
+++ cel/aplicacao/heading.php
@@ -1,85 +1,69 @@
 <?php
-
 session_start();
 
 include("funcoes_genericas.php");
+
 
 checkUserAuthentication("index.php");
 
 //Scenario: access control
 
 /*
-Scenario - User chooses project
-Objective: Allow user to choose a project.
-Context: The User wants to choose a project.
-Preconditions: Login
-Actors: User
-Resources: Projects
-Episodes: The User selects the list of projects a project of which he is not an administrator.
-The user can:
-             - Refresh scenario:
-             - Update lexicon.
+  Scenario - User chooses project
+    Purpose: Allow user to choose a project.
+    Context: The User wants to choose a project.
+    Preconditions: Login
+    Actors: User
+    Resources: Projects
+    Episodes: The User selects the list of projects a project of which he is not an administrator.
+  The user can:
+                 - Refresh scenario:
+                 - Update lexicon.
  */
 
 if (isset($_GET['id_projeto'])) {
-    
     $id_projeto = $_GET['id_projeto'];
-}
-else {
-   //Nothing should be done 
 }
 ?>
 
 <script language="javascript1.3">
 
-    function getIdProject() {
-    
+    function getIDPrj() {
         var selectComboBox = document.forms[0].id_projeto;
         var index = selectComboBox.selectedIndex;
         var id_project = selectComboBox.options[index].value;
-        
         return id_project;
+
     }
 
-    function menuUpdate() {   
-        
-        if (!(document.forms[0].id_projeto.options[0].selected)){
-            
-            top.frames['code'].location.replace('code.php?id_projeto=' + getIdProject());
-            top.frames['text'].location.replace('main.php?id_projeto=' + getIdProject());
+    function atualizaMenu() {   
+        if (!(document.forms[0].id_projeto.options[0].selected))
+        {
+            top.frames['code'].location.replace('code.php?id_projeto=' + getIDPrj());
+            top.frames['text'].location.replace('main.php?id_projeto=' + getIDPrj());
 
-            location.replace('heading.php?id_projeto=' + getIdProject());
-        }
-        else {
-            
+
+            location.replace('heading.php?id_projeto=' + getIDPrj());
+        } else {
+
             location.reload();
         }
-        
         return false;
     }
 
 <?php
-
-
 if (isset($id_projeto)) {   
 
     permissionCheckToProject($_SESSION['id_usuario_corrente'], $id_projeto) or die("Permissao negada");
     ?>
 
-        
-    function setProjectSelected() {
-            
-        var select = document.forms[0].id_projeto;
-            
-        for (var i = 0; i < select.length; i++) {
-                
-            if (select.options[i].value === <?= $id_projeto ?>) {
-                    
-                select.options[i].selected = true;
-                i = select.length;
-            }
-            else {
-                //Nothing should be done
+        function setPrjSelected() {
+            var select = document.forms[0].id_projeto;
+            for (var i = 0; i < select.length; i++) {
+                if (select.options[i].value === <?= $id_projeto ?>) {
+                    select.options[i].selected = true;
+                    i = select.length;
+                }
             }
         }
 
@@ -98,29 +82,25 @@ if (isset($id_projeto)) {
   Episodes: The user clicks on the top menu option:
             If user clicks the add button then INCLUDE SCENARIO
  */
-    
-function newScenario() {
+    function novoCenario() {
+<?php
 
+if (isset($id_projeto)) {
+    ?>
+            var url = 'add_cenario.php?id_projeto=' + '<?= $id_projeto ?>';
     <?php
-    
-    if (isset($id_projeto)) {
-        ?>        
-        var url = 'add_cenario.php?id_projeto=' + '<?= $id_projeto ?>';       
-       <?php
-    }
-    else {
-     
-        ?>            
-         var url = 'add_cenario.php?'    
-        <?php
-     }
-?>
-    
-    var where = '_blank';
-    var window_spec = 'dependent,height=600,width=550,resizable,scrollbars,titlebar';
-    
-    open(url, where, window_spec);
+} else {
+    ?>
+            var url = 'add_cenario.php?'
+    <?php
 }
+?>
+
+
+        var where = '_blank';
+        var window_spec = 'dependent,height=600,width=550,resizable,scrollbars,titlebar';
+        open(url, where, window_spec);
+    }
 
 
 /*
@@ -133,37 +113,29 @@ function newScenario() {
   Episodes: The user clicks on the top menu option:
   If user clicks the add button then INCLUDE LEXICON
  */
-   
-function newLexicon() {
+    function novoLexico() {
+<?php
 
-    <?php
 
-    if (isset($id_projeto)) {
-    
-        ?>
-        var url = 'add_lexico.php?id_projeto=' + '<?= $id_projeto ?>';
-        <?php
-
-    }
-    else {
-        
-        ?>
-        var url = 'add_lexico.php';
-        <?php
-    }
-
+if (isset($id_projeto)) {
     ?>
-
-    var where = '_blank';
-    var window_spec = 'dependent,height=573,width=570,resizable,scrollbars,titlebar';
-        
-    open(url, where, window_spec);
+            var url = 'add_lexico.php?id_projeto=' + '<?= $id_projeto ?>';
+    <?php
+} else {
+    ?>
+            var url = 'add_lexico.php';
+    <?php
 }
+?>
 
-function prjInfo(idprojeto) {
-        
+        var where = '_blank';
+        var window_spec = 'dependent,height=573,width=570,resizable,scrollbars,titlebar';
+        open(url, where, window_spec);
+    }
+
+    function prjInfo(idprojeto) {
         top.frames['text'].location.replace('main.php?id_projeto=' + idprojeto);
-}
+    }
 
 </script>
 
@@ -184,8 +156,9 @@ function prjInfo(idprojeto) {
             text-decoration: none
         }
     </style>
-    <body bgcolor="#ffffff" text="#000000" topmargin="0" leftmargin="0" marginheight="0" marginwidth="0" <?= (isset($id_projeto)) ? "onLoad=\"setProjectSelected();\"" : "" ?>>
-        <form onSubmit="return menuUpdate();">
+    
+    <body bgcolor="#ffffff" text="#000000" topmargin="0" leftmargin="0" marginheight="0" marginwidth="0" <?= (isset($id_projeto)) ? "onLoad=\"setPrjSelected();\"" : "" ?>>
+        <form onSubmit="return atualizaMenu();">
             <table width="100%" cellspacing="0" cellpadding="0">
                 <tr bgcolor="#E0FFFF">
                    <td width="294" height="79" > <!--<img src="Images/Logo.jpg"></td>-->
@@ -201,29 +174,27 @@ if (isset($id_projeto)) {
     $returnCheck = verificaGerente($id_usuario, $id_projeto);
 
     if ($returnCheck != 0) {
-        
-        ?>                             
-         <font color="#FF0033">Administrador</font>
-         
-             <?php
+        ?>
+          <font color="#FF0033">Administrador</font>
+
+
+         <?php
     } 
     else {
-        
-        ?>
-         <font color="#FF0033">Usu&aacute;rio normal</font>
-         
+        ?><font color="#FF0033">Usu�rio normal</font>
+
+
           <?php
     }
  } 
  else {
-       
-     ?>         
-     <?php
-}
+       ?>        
 
+          <?php
+}
        ?>      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Projeto:&nbsp;&nbsp;
 
-       <select name="id_projeto" size="1" onChange="menuUpdate();">
+       <select name="id_projeto" size="1" onChange="atualizaMenu();">
        <option>-- Selecione um Projeto --</option>
 
 
@@ -238,7 +209,7 @@ if (isset($id_projeto)) {
 $SgbdConnect = bd_connect() or die("Erro ao conectar ao SGBD");
 
 // defines consultation
-$commandSQL = "SELECT p.id_projeto, p.nome, pa.gerente
+$comandoSql = "SELECT p.id_projeto, p.nome, pa.gerente
       FROM usuario u, participa pa, projeto p
       WHERE u.id_usuario = pa.id_usuario
       AND pa.id_projeto = p.id_projeto
@@ -246,10 +217,9 @@ $commandSQL = "SELECT p.id_projeto, p.nome, pa.gerente
       ORDER BY p.nome";
 
 // executes consultation
-$requestResultSQL = mysql_query($commandSQL) or die("Erro ao executar query");
+$resultadoRequisicaoSql = mysql_query($comandoSql) or die("Erro ao executar query");
 
-while ($resultArray = mysql_fetch_array($requestResultSQL)) {   
-
+while ($resultArray = mysql_fetch_array($resultadoRequisicaoSql)) {   
     ?>
       <option value="<?= $resultArray['id_projeto'] ?>">
       <?= ($resultArray['gerente'] == 1) ? "*" : "" ?>  <?= $resultArray['nome'] ?></option>
@@ -258,17 +228,14 @@ while ($resultArray = mysql_fetch_array($requestResultSQL)) {
 }
      ?>          
 
-                   
-       </select>&nbsp;&nbsp;           
-       <input type="submit" value="Atualizar">
-            
-                                </td>
+                   </select>&nbsp;&nbsp;
+                  <input type="submit" value="Atualizar">
+            </td>
         </tr>
         <tr bgcolor="#E0FFFF" height="15">
 
         <tr bgcolor="#E0FFFF" height="30">
 
-            
             <td align="right" valign=MIDDLE> <?php
          
             
@@ -287,17 +254,15 @@ Episodes: Appearing in the menu options:
              - Change register
  */ 
             
-if (isset($id_projeto)) { 
+ if (isset($id_projeto)) { 
      
-        ?> <a href="#" onClick="newScenario();">Adicionar Cenário</a>&nbsp;&nbsp;&nbsp; 
-           <a href="#" onClick="newLexicon();">Adicionar Símbolo</a>&nbsp;&nbsp;&nbsp; 
+        ?> <a href="#" onClick="novoCenario();">Adicionar Cenário</a>&nbsp;&nbsp;&nbsp; 
+           <a href="#" onClick="novoLexico();">Adicionar Símbolo</a>&nbsp;&nbsp;&nbsp; 
            <a href="#" title="Informações sobre o Projeto" onClick="prjInfo(<?= $id_projeto ?>);">
                Info</a>&nbsp;&nbsp;&nbsp; 
                <?php
  }
- else{
-     //Nothing should be done
- }
+ 
  
  /*
 Scenario: Register new project
@@ -309,12 +274,10 @@ Resources: System, project's data and database
 Episodes: The User clicks the add project option found on the top menu. 
 */
  
+              ?> <?php
               
- ?> <?php
-              
-             
- ?> <a href="#" onClick="window.open('add_projeto.php', '_blank', 'dependent,height=313,width=550,resizable,scrollbars,titlebar');">                
-     Adicionar Projeto</a>&nbsp;&nbsp;&nbsp; <?php
+             ?> <a href="#" onClick="window.open('add_projeto.php', '_blank', 'dependent,height=313,width=550,resizable,scrollbars,titlebar');">
+                 Adicionar Projeto</a>&nbsp;&nbsp;&nbsp; <?php
                                         
 /*
 Scenario: Remove new project
@@ -327,25 +290,16 @@ Episodes: The administrator clicks the Remove Project in the top menu.
  */
 
 
-  
-     
-if (isset($id_projeto)) {
-    
-    $id_usuario = $_SESSION['id_usuario_corrente'];
-    $returnCheck = verificaGerente($id_usuario, $id_projeto);
+  if (isset($id_projeto)) {
 
-    
-    if ($returnCheck != 0) {
-                        
-        ?> <a href="#" onClick="window.open('remove_projeto.php', '_blank', 'dependent,height=300,width=550,resizable,scrollbars,titlebar');">Remover 
-            Projeto</a>&nbsp;&nbsp;&nbsp; <?php
-     }
-     else{
-         //Nothing should be done
-     }
-  }
-  else {
-      //Nothing should be done
+        $id_usuario = $_SESSION['id_usuario_corrente'];
+
+        $returnCheck = verificaGerente($id_usuario, $id_projeto);
+
+              if ($returnCheck != 0) {
+                        ?> <a href="#" onClick="window.open('remove_projeto.php', '_blank', 'dependent,height=300,width=550,resizable,scrollbars,titlebar');">Remover 
+                         Projeto</a>&nbsp;&nbsp;&nbsp; <?php
+              }
   }
 
   
@@ -360,12 +314,10 @@ Resources: Interface
 Episodes: The user clicks the option to change the interface's registration
  */
   
-     
-  ?> <a href="#" onClick="window.open('Call_UpdUser.php', '_blank', 'dependent,height=300,width=550,resizable,scrollbars,titlebar');">
+       ?> <a href="#" onClick="window.open('Call_UpdUser.php', '_blank', 'dependent,height=300,width=550,resizable,scrollbars,titlebar');">
+           Alterar Cadastro</a>&nbsp;&nbsp;&nbsp; 
 
-      Alterar Cadastro</a>&nbsp;&nbsp;&nbsp; 
-  
-  <a href="mailto:per@les.inf.puc-rio.br">Fale Conosco&nbsp;&nbsp;&nbsp;</a>
+          <a href="mailto:per@les.inf.puc-rio.br">Fale Conosco&nbsp;&nbsp;&nbsp;</a>
 
 
 <?php
@@ -384,9 +336,7 @@ Episodes: The user clicks the Logout option
 
 ?> <a href="logout.php" target="_parent">Sair</a>&nbsp;&nbsp;&nbsp; 
    <a href="ajuda.htm" target="_blank"> Ajuda</a></td>
-                            
-           
-        </tr>
+                            </tr>
                         </table>
                     </td>
                 </tr>
