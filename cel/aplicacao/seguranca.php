5bfba4193edfb870446099c454bead1a20f3e95c
Geisonszo <geisonszo@gmail.com>
2013-09-26 00:14:22 -0300

    Mudancas nos comentarios do arquivo seguranca.php

--- cel/aplicacao/seguranca.php
+++ cel/aplicacao/seguranca.php
@@ -1,31 +1,26 @@
 <?php
-
 /*
- *  Escapa os metacaracteres do PHP
+  Escapes the metacharacters from PHP
  */
-
 function escapa_metacaracteres($string) {
     $string = ereg_replace("[][{}()*+?.\\^$|]", "\\\\0", $string);
     return $string;
 }
 
+/*
+Removes whitespace from the beginning and end of the string.
+Replaces & by amp; (so that no problems to generate the XML).
+Removes tags html and php from string
+Verify if the directive get_magic_quotes_gpc() is activated, if it is, the function stripslashes is used in the string
+ */
+
 function prepara_dado($string) {
-    /*
-     * Remove espaços em branco do inicio e do fim da string
-     * $string = trim( $string );
-     * Substitui o & por amp; (para que não de problemas ao gerar o XML)
-     */
+    
+    $string = trim( $string );
+     
     $string = ereg_replace("&", "&amp;", $string);
 
-    /*
-     *  Retira tags html e php da string
-     */
-
     $string = strip_tags($string);
-
-    /*
-     *  Verifica se a diretiva get_magic_quotes_gpc() esta ativada, se estiver a função stripslashes é utilizada na string
-     */
 
     $string = get_magic_quotes_gpc() ? stripslashes($string) : $string;
     $string = function_exists("mysql_real_escape_string") ? mysql_real_escape_string($string) : mysql_escape_string($string);
