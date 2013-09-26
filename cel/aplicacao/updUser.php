edf457825feafef1475895edc2488c0cc5171bf2
Geisonszo <geisonszo@gmail.com>
2013-09-26 00:35:50 -0300

    Mudanca de nome de variáveis e comnetarios no arquivo updUser.php

--- cel/aplicacao/updUser.php
+++ cel/aplicacao/updUser.php
@@ -5,14 +5,14 @@ include("funcoes_genericas.php");
 include("httprequest.inc");
 include_once("bd.inc");
 
-$id_usuario = $_SESSION['id_usuario_corrente'];
+$id_user = $_SESSION['id_usuario_corrente'];
 
 $SgbdConnect = bd_connect() or die("Erro ao conectar ao SGBD");
 ?>
 
 <html>
     <head>
-        <title>Alterar dados de Usuário</title>
+        <title>Alterar dados de Usu&aacute;rio</title>
         <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
     </head>
 
@@ -21,21 +21,19 @@ $SgbdConnect = bd_connect() or die("Erro ao conectar ao SGBD");
 
 <?php
 /* 
- * Scenario - change cadastrate
- * Objective: To allow to the user actualize changes in the his cadastral data	
- * Context: Open system, User have acess to the system and logged 
- *          User desire change his cadastral data
- * Precondition: User have acess to the system
- * Actors: User, System
- * Resource: Interface
- * Episodes: The user change the desired data
- *           User click int the button of update 
+ Scenario - change cadastrate
+ Objective: To allow to the user actualize changes in the his cadastral data	
+ Context: Open system, User have acess to the system and logged. User desire change his cadastral data
+ Precondition: User have acess to the system
+ Actors: User, System
+ Resource: Interface
+ Episodes: The user change the desired data. User click int the button of update 
  */
 
 $criptPassword = md5($password);
-$commandSQL = "UPDATE usuario SET  nome ='$nome' , login = '$login' , email = '$email' , senha = '$criptPassword' WHERE  id_usuario='$id_usuario'";
+$query_user = "UPDATE usuario SET  nome ='$name' , login = '$login' , email = '$email' , senha = '$criptPassword' WHERE  id_usuario='$id_user'";
 
-mysql_query($commandSQL) or die("<p style='color: red; font-weight: bold; text-align: center'>Erro!Login ja existente!</p><br><br><center><a href='JavaScript:window.history.go(-1)'>Voltar</a></center>");
+mysql_query($query_user) or die("<p style='color: red; font-weight: bold; text-align: center'>Erro!Login ja existente!</p><br><br><center><a href='JavaScript:window.history.go(-1)'>Voltar</a></center>");
 ?>
 
     <center><b>Cadastro atualizado com sucesso!</b></center>
