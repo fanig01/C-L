b9d383921e3fb25435ce0d72f02947de5d5520f2
Geisonszo <geisonszo@gmail.com>
2013-09-26 00:26:38 -0300

    Mudanca de nome de variaveis e comentarios no arquivo teste_daml.php

--- cel/aplicacao/teste_daml.php
+++ cel/aplicacao/teste_daml.php
@@ -1,9 +1,6 @@
 <?php
 
 include 'daml.php';
-/*
- * include 'auxiliar_daml.php';
- */
 include 'auxiliar_bd.php';
 include_once("bd.inc");
 include_once("CELConfig/CELConfig.inc");
@@ -12,20 +9,20 @@ $link = bd_connect();
 
 $site = "http://" . CELConfig_ReadVar("HTTPD_ip") . "/" . CELConfig_ReadVar("CEL_dir_relativo") . CELConfig_ReadVar("DAML_dir_relativo_ao_CEL");
 $dir = CELConfig_ReadVar("DAML_dir_relativo_ao_CEL");
-$arquivo = nome_arquivo_daml();
+$file = nome_arquivo_daml();
 
-$i = array("title" => "Ontologia de teste",
+$input = array("title" => "Ontologia de teste",
     "creator" => "Pedro",
     "description" => "teste de tradução de léxico para ontologia",
     "subject" => "",
     "versionInfo" => "1.1");
 
-$lista_conceitos = get_lista_de_conceitos();
-$lista_relacoes = get_lista_de_relacoes();
-$lista_axiomas = get_lista_de_axiomas();
+$list_concept = get_lista_de_conceitos();
+$list_relationship = get_lista_de_relacoes();
+$list_axiom = get_lista_de_axiomas();
 
 
-$daml = salva_daml($site, $dir, $arquivo, $i, $lista_conceitos, $lista_relacoes, $lista_axiomas);
+$daml = salva_daml($site, $dir, $file, $input, $list_concept, $list_relationship, $list_axiom);
 
 if (!$daml) {
     print 'Erro ao exportar ontologia para DAML!';
