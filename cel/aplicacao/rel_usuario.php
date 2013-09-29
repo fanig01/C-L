cbfe3307ad88afd38f9baa647e16b727cfd11c70
Thaiane Braga <thaiane.unb@hotmail.com>
2013-09-29 17:39:25 -0300

    Aplicação das técnicas de programação no arquivo rel_usuario.php

--- cel/aplicacao/rel_usuario.php
+++ cel/aplicacao/rel_usuario.php
@@ -13,14 +13,14 @@ $SgbdConnect = bd_connect() or die("Erro ao conectar ao SGBD");
 if (isset($submit)) {
     
     $commandSQL = "DELETE FROM participa
-          WHERE id_usuario != " . $_SESSION['id_usuario_corrente'] . "
-          AND id_projeto = " . $_SESSION['id_projeto_corrente'];
+                   WHERE id_usuario != " . $_SESSION['id_usuario_corrente'] . "
+                   AND id_projeto = " . $_SESSION['id_projeto_corrente'];
     
     mysql_query($commandSQL) or die("Erro ao executar a query de DELETE");
 
-    $n_sel = count($users);
+    $selectedNumber = count($users);
     
-    for ($i = 0; $i < $n_sel; $i++) {
+    for ($i = 0; $i < $selectedNumber; $i++) {
         
         $commandSQL = "INSERT INTO participa (id_usuario, id_projeto)
                        VALUES (" . $users[$i] . ", " . $_SESSION['id_projeto_corrente'] . ")";
@@ -51,25 +51,31 @@ else {
             <script language="javascript1.3" src="MSelect.js"></script>
             <script language="javascript1.3">
 
-                function createMSelect() {
-                    var usr_lselect = document.forms[0].elements['usuarios[]'];
-                    var usr_rselect = document.forms[0].usuarios_r;
-                    var usr_l2r = document.forms[0].usr_l2r;
-                    var usr_r2l = document.forms[0].usr_r2l;
-                    var MS_usr = new MSelect(usr_lselect, usr_rselect, usr_l2r, usr_r2l);
-                }
-
-                function selAll() {
-                    var usuarios = document.forms[0].elements['usuarios[]'];
-                    for (var i = 0; i < usuarios.length; i++)
-                        usuarios.options[i].selected = true;
-                }
-
+                
+    function createMSelect() {
+                    
+        var usr_lselect = document.forms[0].elements['usuarios[]'];
+        var usr_rselect = document.forms[0].usuarios_r;
+        var usr_l2r = document.forms[0].usr_l2r;
+        var usr_r2l = document.forms[0].usr_r2l;
+        var MS_usr = new MSelect(usr_lselect, usr_rselect, usr_l2r, usr_r2l);
+    }
             
+    function selAll() {
+                    
+        var usuarios = document.forms[0].elements['usuarios[]'];
+                    
+        for (var i = 0; i < usuarios.length; i++) {
+                        
+        usuarios.options[i].selected = true;
+        
+        }
+               
+     }
+                    
             </script>
-            <style>
-            
-                <!--
+            <style>                           
+                <!--               
                 select {
                     width: 200;
                     background-color: #CCFFFF
@@ -78,13 +84,12 @@ else {
             
             </style>
         
-        </head>
-        
+        </head>       
         <body onLoad="createMSelect();">
-           
+                       
             <h4>Selecione os usu&aacute;rios para participar do projeto "<span style="color: orange"><?= simple_query("nome", "projeto", "id_projeto = " . $_SESSION['id_projeto_corrente']) ?></span>":</h4>
             
-            <p style="color: red">Mantenha <strong>CTRL</strong> pressionado para selecionar múltiplas opções</p>
+            <p style="color: red">Mantenha <strong>CTRL</strong> pressionado para selecionar m&uacute;ltiplas op&ccedil;&otilde;es</p>
             <form action="" method="post" onSubmit="selAll();">
                 <table cellspacing="8" width="100%">
                     <tr>
@@ -118,6 +123,7 @@ from the list of users who do not belong to this project and click the button <-
         To update the relationships, the administrator clicks the Refresh button
 */   
 
+    
     $commandSQL = "SELECT u.id_usuario, login
           FROM usuario u, participa p
           WHERE u.id_usuario = p.id_usuario
@@ -127,21 +133,17 @@ from the list of users who do not belong to this project and click the button <-
     $requestResultSQL = mysql_query($commandSQL) or die("Erro ao enviar a query");
    
     
+    
     while ($resultArray = mysql_fetch_array($requestResultSQL)) {
     
-        ?>
-                               
-            <option value="<?= $resultArray['id_usuario'] ?>"><?= $resultArray['login'] ?></option>
-
+        ?>                                      
+                                <option value="<?= $resultArray['id_usuario'] ?>"><?= $resultArray['login'] ?></option>
                                    <?php
- 
                                     ?>
-
                                     <?php
     }
                                 ?>
-
-                          
+                        
                             </select>
                         </td>
                         <td>
@@ -152,21 +154,26 @@ from the list of users who do not belong to this project and click the button <-
 
     <?php
     
-    $subq = "SELECT id_usuario FROM participa where participa.id_projeto =" . $_SESSION['id_projeto_corrente'];
-    $subqrr = mysql_query($subq) or die("Erro ao enviar a subquery");
-    $resultadosubq = "(0)";
+    $subQuery = "SELECT id_usuario FROM participa WHERE participa.id_projeto =" . $_SESSION['id_projeto_corrente'];
+    $subRequestResultSQL = mysql_query($subQuery) or die("Erro ao enviar a subquery");
+    $resultSubQuery = "(0)";
     
-    if ($subqrr != 0) {
+    if ($subRequestResultSQL != 0) {
     
-        $row = mysql_fetch_row($subqrr);
-        $resultadosubq = "( $row[0]";
-        while ($row = mysql_fetch_row($subqrr))
-            $resultadosubq = "$resultadosubq , $row[0]";
-        $resultadosubq = "$resultadosubq )";
+        $row = mysql_fetch_row($subRequestResultSQL);
+        $resultSubQuery = "( $row[0]";
+        
+        while ($row = mysql_fetch_row($subRequestResultSQL))
+            
+            $resultSubQuery = "$resultSubQuery , $row[0]";        
+            $resultSubQuery = "$resultSubQuery )";
+    }
+    else {
+        //Nothing should be done
     }
     
     $commandSQL = "SELECT usuario.id_usuario, usuario.login 
-                  FROM usuario where usuario.id_usuario not in " . $resultadosubq;
+                  FROM usuario where usuario.id_usuario not in " . $resultSubQuery;
     
     echo($commandSQL);
     
@@ -176,7 +183,8 @@ from the list of users who do not belong to this project and click the button <-
         ?>
                                     
                                 
-            <option value="<?= $resultArray['id_usuario'] ?>"><?= $resultArray['login'] ?></option>
+            
+                                <option value="<?= $resultArray['id_usuario'] ?>"><?= $resultArray['login'] ?></option>
 
                                     <?php
 
@@ -210,7 +218,7 @@ from the list of users who do not belong to this project and click the button <-
             
             </form>
            
-            <br><i><a href="showSource.php?file=rel_usuario.php">Veja o código fonte!</a></i>
+            <br><i><a href="showSource.php?file=rel_usuario.php">Veja o c&oacute;digo fonte!</a></i>
         
         </body>
     </html>
