e50a6272922c4ccb55c6c42ce7d69321b314837f
Tomaz Martins <tomaz.unb@outlook.com>
2013-10-20 18:36:13 -0200

    Alteração na identação e espaçamento

--- cel/aplicacao/bd_class.php
+++ cel/aplicacao/bd_class.php
@@ -1,36 +1,38 @@
 <?php
 
 /*
-  ##   Simple PostgreSQL Abstraction Layer v1.0
-  ##   by Cem �EV�K <cemc@linux.org.tr>
-  ##   Sturday, January 06, 2001
-  ##
-  ##   Easy way to access database and store fetched results.
-  ##   Database connection and query operation implemented in a separate
-  ##   class to play with multiple query results simultaneously over
-  ##   the same connection.
-  ##
-  ##   You can implement your own abstraction layer for other DBMSes that
-  ##   PHP supports.
-  ##
-  ##   If you find a way/idea to run this software better,
-  ##   please send it to author.
-  ##   Tanks to my colleagues.
-  ##
-  ##   Usage :
-  ##          $DB = new PGDB ();
-  ##          $sql = new QUERY ($DB);
-  ##          $sql->execute ("select name, surname, email from users where " .
-  ##                "username = '$username'");
-  ##          if ($sql->getntuples () == 0) {
-  ##                 echo "No records found !";
-  ##                 $DB->close ();
-  ##                 exit;
-  ##          }
-  ##          $record = $sql->gofirst ();
-  ##          $name = $record['name'];
-  ##          echo "Realname of ($username) : " . $name;
-  ##          $DB->close ();
+   Simple PostgreSQL Abstraction Layer v1.0
+   by Cem �EV�K <cemc@linux.org.tr>
+   Sturday, January 06, 2001
+
+   Easy way to access database and store fetched results.
+   Database connection and query operation implemented in a separate
+   class to play with multiple query results simultaneously over
+   the same connection.
+
+   You can implement your own abstraction layer for other DBMSes that
+   PHP supports.
+
+   If you find a way/idea to run this software better,
+   please send it to author.
+   Tanks to my colleagues.
+
+   Usage :
+          $DB = new PGDB ();
+          $sql = new QUERY ($DB);
+          $sql->execute ("select name, surname, email from users where " .
+                "username = '$username'");
+
+          if ($sql->getntuples () == 0) {
+                 echo "No records found !";
+                 $DB->close ();
+                 exit;
+          }
+ 
+          $record = $sql->gofirst ();
+          $name = $record['name'];
+          echo "Realname of ($username) : " . $name;
+          $DB->close ();
  */
 
 /* Aten��o!!!
@@ -83,24 +85,33 @@ class PGDB extends Abstract_DB {
     }
 
     function _PGDB() {
+        
         $this->close();
+        
     }
 
     function open($dbname, $user, $passwd, $host, $port) {
+        
         $this->db_linkid = bd_connect() or die("Erro na conex�o � BD : " . mysql_error());
 
 //              if( $this->db_linkid && mysql_select_db(CELConfig_ReadVar("BD_database") . "" ) )
+        
         if ($this->db_linkid) {
+            
             return $this->db_linkid;
+            
         } else {
+            
             return(FALSE);
+            
         }
     }
 
     function close() {
+        
         return mysql_close($this->db_linkid);
+        
     }
-
 }
 
 class QUERY {
@@ -115,86 +126,127 @@ class QUERY {
 ##PUBLIC##
 
     function QUERY($pdbobject) {
-        if ($pdbobject)
+        
+        if ($pdbobject) {
+            
             $this->associate($pdbobject);
+            
+        }
     }
 
     function associate($pdbobject) {
+        
         $this->dbobject = $pdbobject;
+        
     }
 
     function execute($querystring) {
+        
         //echo( $querystring);
         $this->operationresult = mysql_query($querystring) or die(mysql_error() . "<br>" . $querystring);
         return $this->operationresult;
+        
     }
 
     function getntuples() {
+        
         $this->ntuples = mysql_numrows($this->operationresult);
         return $this->ntuples;
+        
     }
 
     function getfieldname($fieldnumber) {
+        
         return mysql_fieldname($this->operationresult, $fieldnumber);
+        
     }
 
     function readrow() {
+        
         $this->resultset = mysql_fetch_array($this->operationresult);
         return ($this->currentresultset = $this->resultset);
+        
     }
 
     function gofirst() {
+        
         $this->currentrow = 0;
         return $this->readrow();
+        
     }
 
     function golast() {
+        
         $this->currentrow = ($this->getntuples()) - 1;
         return $this->readrow();
+        
     }
 
     function getLastId() {
+        
         return mysql_insert_id($this->dbobject->db_linkid);
+        
     }
 
     function gonext() {
+        
         $this->currentrow++;
+        
         if ($this->currentrow < $this->getntuples()) {
+            
             $this->resultset = $this->readrow();
             return $this->resultset;
-        }
-        else
+            
+        } else {
+            
             return "LAST_RECORD_REACHED";
+        }
     }
 
     function goprevious() {
+        
         $this->currentrow--;
+        
         if ($this->currentrow >= 0) {
+            
             $this->resultset = $this->readrow();
             return $this->resultset;
-        }
-        else
+            
+        } else {
+            
             return "FIRST_RECORD_REACHED";
+        }
     }
 
     function beginTransaction() {
-        if (!$this->execute("BEGIN"))
+        
+        if (!$this->execute("BEGIN")) {
+            
             return false;
+        }
+        
         return true;
     }
 
     function commitTransaction() {
-        if (!$this->execute("COMMIT"))
+        
+        if (!$this->execute("COMMIT")) {
+            
             return false;
+        }
+        
         return true;
     }
 
     function rollbackTransaction() {
-        if (!$this->execute("ROLLBACK"))
+        
+        if (!$this->execute("ROLLBACK")) {
+            
             return false;
+        }
+        
         return true;
     }
-
 }
 
 ?>
