13795452b5da13380d64f7c23645fa864dff1cd9
Tomaz Martins <tomaz.unb@outlook.com>
2013-10-20 15:35:25 -0200

    Alteração na identação e espaçamento
    Melhora da clareza do código

--- cel/aplicacao/algoritmo_inicio.php
+++ cel/aplicacao/algoritmo_inicio.php
@@ -10,10 +10,12 @@ include_once "script_bd2.php";
 //mysql_close($link);
 $link = bd_connect();
 
-$list = verifica_tipo();
+$list = vezrifica_tipo();
 
 if (is_array($list)) {
+    
     foreach ($list as $id) {
+        
         $lexic = obter_lexico($id);
         $aux[] = $lexic["nome"];
     }
@@ -21,11 +23,15 @@ if (is_array($list)) {
     $_SESSION["lista"] = $aux;
     $_SESSION["job"] = "type";
     $_SESSION["nome1"] = 1;
+    
     ?>
+
     <script language="javascript">
         window.location = "auxiliar_interface.php";
     </script>
+    
     <?php
+    
     exit();
 }
 
@@ -43,7 +49,9 @@ $_SESSION["salvar"] = "FALSE";
 
 
 if ($_POST["load"] == "FALSE") {
+    
     converte_impactos();
+    
     $_SESSION["lista_de_conceitos"] = array();
     $_SESSION["lista_de_relacoes"] = array();
     $_SESSION["lista_de_axiomas"] = array();
@@ -57,27 +65,33 @@ if ($_POST["load"] == "FALSE") {
     $_SESSION["index5"] = 0;
     $_SESSION["index6"] = 0;
     $_SESSION["index7"] = 0;
+    
 } else {
+    
     $_SESSION["lista_de_relacoes"] = get_lista_de_relacoes();
     $_SESSION["lista_de_conceitos"] = get_lista_de_conceitos();
     $_SESSION["lista_de_axiomas"] = get_lista_de_axiomas();
 
     $_SESSION["funcao"] = get_funcao();
 
-
-
     $indices = get_indices();
+    
     if (count($indices) == 5) {
+        
         $_SESSION["index1"] = $indices['index1']; //Sujeito
         $_SESSION["index3"] = $indices['index3']; //Verbo
         $_SESSION["index4"] = $indices['index4']; //Estado
         $_SESSION["index5"] = $indices['index5']; //Organizacao
+        
     } else {
+        
         $_SESSION["index1"] = 0; //Sujeito
         $_SESSION["index3"] = 0; //Verbo
         $_SESSION["index4"] = 0; //Estado
         $_SESSION["index5"] = 0; //Organizacao
+        
     }
+    
     $_SESSION["index2"] = 0;
     $_SESSION["index6"] = 0;
     $_SESSION["index7"] = 0;
