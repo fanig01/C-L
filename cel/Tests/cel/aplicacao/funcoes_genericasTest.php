3ec9b31d688782410d6e819a3a623e7e028547c4
fanig01 <fanig_01@hotmail.com>
2013-10-20 21:53:05 -0200

    Adicionado teste do arquivo funcoes genericas e feito primeiro teste de uma funcao, teste da funcao CheckUserAuthentication

---
+++ cel/Tests/cel/aplicacao/funcoes_genericasTest.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once '../../cel/aplicacao/funcoes_genericas.php';
+
+
+class funcoes_genericasTest extends PHPUnit_Framework_TestCase {
+    
+    public $test;
+    
+    protected function setUp() {
+        $this->object = new funcoes_genericas();
+    }
+    
+    protected function tearDown() {
+        
+    }
+    
+    public function testCheckUserNotAuthentication() {
+        $actual = $this->object->checkUserAuthentication($url);
+        $expected = NULL;
+        $this->assertEquals($expected, $actual);
+    }
+}
+?>
