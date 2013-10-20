3bf5caf0197319d76792222a1726a1c65a691188
Tomaz Martins <tomaz.unb@outlook.com>
2013-10-20 14:31:48 -0200

    Alterações na identação e espaçamento

--- cel/aplicacao/add_usuario.php
+++ cel/aplicacao/add_usuario.php
@@ -5,29 +5,31 @@ session_start();
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 
 <?php
+
 include("funcoes_genericas.php");
+include("httprequest.inc");
 include_once("bd.inc");
 
 $primeira_vez = "true";
 
-include("httprequest.inc");
-
-if (isset($submit)) {   // Se chamado pelo botao de submit
+if (isset($submit)) {
+    
     $primeira_vez = "false";
-    // ** Cenario "Inclusao de Usuario Independente" **
-    // O sistema checa se todos os campos estao preenchidos. Se algum nao estiver, o
-    // sistema avisa pro usuario que todos os campos devem ser preenchidos.
+    
     if ($name == "" || $email == "" || $login == "" || $password == "" || $senha_conf == "") {
+        
         $p_style = "color: red; font-weight: bold";
         $p_text = "Por favor, preencha todos os campos.";
         recharge("?p_style=$p_style&p_text=$p_text&nome=$name&email=$email&login=$login&senha=$password&senha_conf=$senha_conf&novo=$novo");
+        
     } else {
 
-        // Testa se as senhas fornecidas pelo usuario sao iguais.
         if ($password != $senha_conf) {
+            
             $p_style = "color: red; font-weight: bold";
             $p_text = "Senhas diferentes. Favor preencher novamente as senhas.";
             recharge("?p_style=$p_style&p_text=$p_text&nome=$name&email=$email&login=$login&novo=$novo");
+            
         } else {
 
             // ** Cenario "Inclusao de Usuario Independente" **
@@ -55,7 +57,9 @@ if (isset($submit)) {   // Se chamado pelo botao de submit
             $SgbdConnect = bd_connect() or die("Erro ao conectar ao SGBD");
             $commandSQL = "SELECT id_usuario FROM usuario WHERE login = '$login'";
             $requestResultSQL = mysql_query($commandSQL) or die("Erro ao enviar a query");
-            if (mysql_num_rows($requestResultSQL)) {        // Se ja existe algum usuario com este login
+            
+            if (mysql_num_rows($requestResultSQL)) {
+                
 //                $p_style = "color: red; font-weight: bold";
 //                $p_text = "Login j� existente no sistema. Favor escolher outro login.";
 //                recarrega("?p_style=$p_style&p_text=$p_text&nome=$name&email=$email&senha=$senha&senha_conf=$senha_conf&novo=$novo");
@@ -76,8 +80,11 @@ if (isset($submit)) {   // Se chamado pelo botao de submit
                 </script>
 
                 <?php
+                
                 recharge("?novo=$novo");
+                
             } else {    // Cadastro passou por todos os testes -- ja pode ser incluido na BD
+                
                 /* Substitui todas as ocorrencias de ">" e "<" por " " */
                 $name = str_replace(">", " ", str_replace("<", " ", $name));
                 $login = str_replace(">", " ", str_replace("<", " ", $login));
@@ -89,14 +96,18 @@ if (isset($submit)) {   // Se chamado pelo botao de submit
                 mysql_query($commandSQL) or die("Erro ao cadastrar o usuario");
                 recharge("?cadastrado=&novo=$novo&login=$login");
             }
+            
         }   // else
+        
     }   // else
-} elseif (isset($cadastrado)) {
+    
+} else if (isset($cadastrado)) {
 
     // Cadastro concluido. Dependendo de onde o usuario veio,
     // devemos manda-lo para um lugar diferente.
 
     if ($novo == "true") {      // Veio da tela inicial de login
+
         // ** Cenario "Inclusao de Usuario Independente" **
         // O usuario acabou de cadastrar-se no sistema, devemos
         // redireciona-lo para a parte de inclusao de projetos
@@ -111,7 +122,8 @@ if (isset($submit)) {   // Se chamado pelo botao de submit
 // Recursos:  Interface, Banco de Dados	
 // Epis�dios:  Caso aquele login digitado n�o exista, o sistema cadastra esse usu�rio 
 //               como administrador no banco de dados,  possibilitando:
-//              - Redirecion�-lo  para a interface de CADASTRAR NOVO PROJETO; 
+//              - Redirecion�-lo  para a interface de CADASTRAR NOVO PROJETO;
+        
         $id_usuario_corrente = simple_query("id_usuario", "usuario", "login = '$login'");
         session_register("id_usuario_corrente");
         ?>
@@ -127,6 +139,7 @@ if (isset($submit)) {   // Se chamado pelo botao de submit
         </script>
 
         <?php
+        
     } else {
 
         // ** Cenario "Edicao de Usuario" **
@@ -137,8 +150,10 @@ if (isset($submit)) {   // Se chamado pelo botao de submit
         $SgbdConnect = bd_connect() or die("Erro ao conectar ao SGBD");
         // $login eh o login do usuario incluido, passado na URL
         $id_usuario_incluido = simple_query("id_usuario", "usuario", "login = '$login'");
+        
         $commandSQL = "INSERT INTO participa (id_usuario, id_projeto)
           VALUES ($id_usuario_incluido, " . $_SESSION['id_projeto_corrente'] . ")";
+        
         mysql_query($commandSQL) or die("Erro ao inserir na tabela participa");
 
         $nome_usuario = simple_query("nome", "usuario", "id_usuario = $id_usuario_incluido");
@@ -154,13 +169,17 @@ if (isset($submit)) {   // Se chamado pelo botao de submit
 
         <?php
     }
+    
 } else {    // Script chamado normalmente
+    
     if (empty($p_style)) {
+        
         $p_style = "color: green; font-weight: bold";
         $p_text = "Favor preencher os dados abaixo:";
     }
 
     if ($primeira_vez) {
+        
         $email = "";
         $login = "";
         $name = "";
@@ -177,36 +196,39 @@ if (isset($submit)) {   // Se chamado pelo botao de submit
         <body>
             <script language="JavaScript">
                 <!--
-                function verifyEmail(form)
-                {
+                
+                function verifyEmail(form) {
+                    
                     email = form.email.value;
                     //verifica se o email contem um @
                     i = email.indexOf("@");
-                    if (i == -1)
-                    {
+                    
+                    if (i == -1) {
+                        
                         alert('Aten��o: o E-mail digitado n�o � v�lido.');
                         return false;
                     }
+                    
                 }
 
                 function checkEmail(email) {
-                    if (email.value.length > 0)
-                    {
-                        if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email.value))
-                        {
+                    
+                    if (email.value.length > 0) {
+                        
+                        if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email.value)) {
                             return (true)
                         }
+                        
                         alert("Aten��o: o E-mail digitado n�o � v�lido.")
                         email.focus();
                         email.select();
                         return (false)
+                        
                     }
+                    
                 }
 
                 //-->
-
-
-
             </SCRIPT>
 
             <p style="<?= $p_style ?>"><?= $p_text ?></p>
