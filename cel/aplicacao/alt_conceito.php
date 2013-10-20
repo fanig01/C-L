e8273952ec45f4cc38cac512b8a2f67c0828c8ea
Thaiane Braga <thaiane.unb@hotmail.com>
2013-10-20 16:19:56 -0200

    Alterações no arquivo alt_cenario.php

--- cel/aplicacao/alt_conceito.php
+++ cel/aplicacao/alt_conceito.php
@@ -1,21 +1,22 @@
 <?php
-/* vim: set expandtab tabstop=4 shiftwidth=4: */
-
-// alt_conceito.php: Este script faz um pedido de alteracao de um conceito do projeto.
-// O usuario recebe um form com o conceito corrente (ou seja com seus campos preenchidos)
-// e podera fazer	alteracoes em todos os campos menos no nome.Ao final a tela principal
-// retorna para a tela de inicio e a arvore e fechada.O form de alteracao tb e fechado.
-// Arquivo chamador: main.php
 session_start();
+
 include("funcoes_genericas.php");
 include("httprequest.inc");
 include_once("bd.inc");
 
-checkUserAuthentication("index.php"); // Checa se o usuario foi autenticado
-// Conecta ao SGBD
+/*
+ alt_conceito.php: This script makes a request for alteration of a project concept.
+ The user receives a form with the current concept and podeá make changes in all 
+ fields except the name. At the end, the main screen returns to the start screen and the tree is closed.
+*/
+
+checkUserAuthentication("index.php"); 
+
 $SgbdConnect = bd_connect() or die("Erro ao conectar ao SGBD");
 
-if (isset($submit)) {       // Script chamado atraves do submit do formulario
+// Script called through the form's submit
+if (isset($submit)) {      
     inserirPedidoAlterarConceito($_SESSION['id_projeto_corrente'], $id_conceito, $name, $description, $namespace, $justificativa, $_SESSION['id_usuario_corrente']);
     ?>
 
@@ -26,7 +27,7 @@ if (isset($submit)) {       // Script chamado atraves do submit do formulario
 
     </script>
 
-    <h4>Opera��o efetuada com sucesso!</h4>
+    <h4>Opera&ccedil;&atilde;o efetuada com sucesso!</h4>
 
     <script language="javascript1.3">
 
@@ -35,25 +36,28 @@ if (isset($submit)) {       // Script chamado atraves do submit do formulario
     </script>
 
     <?php
-} else { // Script chamado atraves do link no cenario corrente
+} 
+else { 
+// Script called through the link in current scenario
+    
     $nameProject = simple_query("nome", "projeto", "id_projeto = " . $_SESSION['id_projeto_corrente']);
-
     $commandSQL = "SELECT * FROM conceito WHERE id_conceito = $id_conceito";
     $requestResultSQL = mysql_query($commandSQL) or die("Erro ao executar a query");
     $resultArray = mysql_fetch_array($requestResultSQL);
 
-// Cen�rio -    Alterar Conceito 
-//Objetivo:	Permitir a altera��o de um conceito por um usu�rio
-//Contexto:	Usu�rio deseja alterar conceito previamente cadastrado
-//              Pr�-Condi��o: Login, Cen�rio cadastrado no sistema
-//Atores:	Usu�rio
-//Recursos:	Sistema, dados cadastrados
-//Epis�dios:	O sistema fornecer� para o usu�rio a mesma tela de INCLUIR CEN�RIO,
-//              por�m com os seguintes dados do cen�rio a ser alterado preenchidos
-//              e edit�veis nos seus respectivos campos: Objetivo, Contexto, Atores, Recursos e Epis�dios.
-//              Os campos Projeto e T�tulo estar�o preenchidos, mas n�o edit�veis.
-//              Ser� exibido um campo Justificativa para o usu�rio colocar uma
-//              justificativa para a altera��o feita.
+/*
+Scenario: Changing Concept
+Objective: Allow changing a concept for a user
+Context: User want to change concept previously registered
+Precondition: login and setting registered in the system
+Actors: User
+Resources: System and registered data
+Episodes: The system will provide to the user the same screen SCENE INCLUDED,
+but with the following scenario data to be changed and filled editable in their
+respective fields: Objectives, Context, Actors, Resources and Episodes.
+The Project and Title fields will be filled, but not editable. 
+Displays a field Rationale for the user to place a justification for the change made.
+ */
     ?>
 
     <html>
@@ -90,11 +94,11 @@ if (isset($submit)) {       // Script chamado atraves do submit do formulario
                         <td><textarea name="justificativa" cols="48" rows="2"></textarea></td>
                     </tr>
                     <tr>
-                        <td align="center" colspan="2" height="60"><input name="submit" type="submit" value="Alterar Cen�rio" onClick="updateOpener()"></td>
+                        <td align="center" colspan="2" height="60"><input name="submit" type="submit" value="Alterar Cen&aacute;rio" onClick="updateOpener()"></td>
                     </tr>
                 </table>
             </form>
-            <br><i><a href="showSource.php?file=alt_cenario.php">Veja o c�digo fonte!</a></i>
+            <br><i><a href="showSource.php?file=alt_cenario.php">Veja o c&oacute;digo fonte!</a></i>
         </body>
     </html>
 
